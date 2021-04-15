d21088c85741f2e3550c4888a9f2eca006672608
Raj.Sojitra <raj.sojitra@openxcellinc.com>
2021-04-15 11:35:06 +0530

    first commit to react images

---
+++ src/components/Searchbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Searchbar extends React.Component {
+
+    state = {term : ''};  // control events
+    // onInputChange(event){
+    //     console.log(event.target.value);    // uncontrol events
+    // }
+
+    onFormSubmit= (evnt) => {                       
+        evnt.preventDefault();
+        //console.log(this.state.term);
+        this.props.onSubmit(this.state.term);
+    }
+
+    render(){
+        return (
+            <div className="ui segment" >
+                <form onSubmit={this.onFormSubmit} className="ui form" >
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" value={this.state.term} onChange={(e) => this.setState({term : e.target.value})}/>
+                    </div>
+                </form>
+            </div>
+        );
+    };
+}
+
+export default Searchbar;
